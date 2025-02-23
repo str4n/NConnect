edac8feaaaf133956d8223ad55c69286423f4ce4
Kamil <Str4ngerGameOne@gmail.com>
2025-02-23 23:18:35 +0100

    feat: API gateway using YARP & saga service.

---
+++ src/Aspire/NConnect.Aspire.AppHost/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Projects;
+
+namespace NConnect.Aspire.AppHost;
+
+internal static class Extensions
+{
+    public static IDistributedApplicationBuilder AddServices(this IDistributedApplicationBuilder builder)
+    {
+        var saga = builder.AddProject<NConnect_Services_Saga>(GetServiceName<NConnect_Services_Saga>());
+        var apiGateway = builder.AddProject<NConnect_APIGateway>(GetServiceName<NConnect_APIGateway>())
+            .WithReference(saga);
+        
+        return builder;
+    }
+    
+    private static string GetServiceName<T>() => typeof(T).Name.Split('_').Last();
+}
